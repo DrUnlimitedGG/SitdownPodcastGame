80c07398f45d54327dec5030a21b355a80ca98f9
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-18 00:29:32 -0400

    Making the other sign work and also adding some custom commands in the admin system

---
+++ src/ServerScriptService/Adonis_Loader/Config/Plugins/Server-Flashsign.lua
@@ -0,0 +1,41 @@
+--[[
+	SERVER PLUGINS' NAMES MUST START WITH "Server:" OR "Server-"
+	CLIENT PLUGINS' NAMES MUST START WITH "Client:" OR "Client-"
+
+	Plugins have full access to the server/client tables and most variables.
+
+	You can use the MakePluginEvent to use the script instead of setting up an event.
+	PlayerChatted will get chats from the custom chat and nil players.
+	PlayerJoined will fire after the player finishes initial loading
+	CharacterAdded will also fire after the player is loaded, it does not use the CharacterAdded event.
+
+	service.Events.PlayerChatted(function(plr, msg)
+		print(msg..' from '..plr.Name..' Example Plugin')
+	end)
+
+	service.Events.PlayerAdded(function(p)
+		print(p.Name..' Joined! Example Plugin')
+	end)
+
+	service.Events.CharacterAdded(function(p)
+		server.RunCommand('name',plr.Name,'BobTest Example Plugin')
+	end)
+
+--]]
+
+return function(Vargs)
+	local server, service = Vargs.Server, Vargs.Service
+
+	server.Commands.Flashsign = {
+		Prefix = server.Settings.Prefix;	-- Prefix to use for command
+		Commands = {"flashsign"};	-- Commands
+		Args = {};	-- Command arguments
+		Description = "Flashes the Syndicate Sitdown sign";	-- Command Description
+		Hidden = false; -- Is it hidden from the command list?
+		Fun = false;	-- Is it fun?
+		AdminLevel = "Admins";	    -- Admin level; If using settings.CustomRanks set this to the custom rank name (eg. "Baristas")
+		Function = function(plr)    -- Function to run for command
+			require(10098815141).Load(false, "SitdownSign")
+		end
+	}
+end
---
+++ src/ServerScriptService/Adonis_Loader/Config/Plugins/Server-Volume.lua
@@ -0,0 +1,44 @@
+--[[
+	SERVER PLUGINS' NAMES MUST START WITH "Server:" OR "Server-"
+	CLIENT PLUGINS' NAMES MUST START WITH "Client:" OR "Client-"
+
+	Plugins have full access to the server/client tables and most variables.
+
+	You can use the MakePluginEvent to use the script instead of setting up an event.
+	PlayerChatted will get chats from the custom chat and nil players.
+	PlayerJoined will fire after the player finishes initial loading
+	CharacterAdded will also fire after the player is loaded, it does not use the CharacterAdded event.
+
+	service.Events.PlayerChatted(function(plr, msg)
+		print(msg..' from '..plr.Name..' Example Plugin')
+	end)
+
+	service.Events.PlayerAdded(function(p)
+		print(p.Name..' Joined! Example Plugin')
+	end)
+
+	service.Events.CharacterAdded(function(p)
+		server.RunCommand('name',plr.Name,'BobTest Example Plugin')
+	end)
+
+--]]
+
+return function(Vargs)
+	local server, service = Vargs.Server, Vargs.Service
+
+	server.Commands.Volume = {
+		Prefix = server.Settings.Prefix;	-- Prefix to use for command
+		Commands = {"backgroundvolume"};	-- Commands
+		Args = {"volume (0 to 1)"};	-- Command arguments
+		Description = "Changes the volume of the background music";	-- Command Description
+		Hidden = false; -- Is it hidden from the command list?
+		Fun = false;	-- Is it fun?
+		AdminLevel = "Admins";	    -- Admin level; If using settings.CustomRanks set this to the custom rank name (eg. "Baristas")
+		Function = function(plr,args)    -- Function to run for command
+			local sign = workspace:FindFirstChild("Background")
+			sign.Volume = args[1]
+			
+			server.Functions.Hint("Changed volume to "..args[1], {plr})
+		end
+	}
+end
