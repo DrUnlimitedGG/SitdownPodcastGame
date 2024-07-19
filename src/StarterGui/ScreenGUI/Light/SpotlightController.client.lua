c2dd0bd7e6c06ba6ee1b32bd9608774b33b55ce2
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-19 13:38:47 -0400

    finished lights gui

---
+++ src/StarterGui/ScreenGUI/Light/SpotlightController.client.lua
@@ -0,0 +1,28 @@
+local uis = game:GetService("UserInputService")
+
+local slider = script.Parent.SpotlightSlider
+local sliderButton = slider.SliderButton
+local sliderDragging = false
+
+sliderButton.MouseButton1Down:Connect(function()
+	sliderDragging = true
+end)
+
+uis.InputEnded:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		sliderDragging = false
+	end
+end)
+
+uis.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement then
+		if sliderDragging == true then
+			local mouseLoc = uis:GetMouseLocation()
+			local relativePos = mouseLoc - slider.AbsolutePosition
+			local percentage = math.clamp(relativePos.X/slider.AbsoluteSize.X,0,1)
+			sliderButton.Position = UDim2.new(percentage,0,.5,0)
+			
+			wait(0.05)
+		end
+	end
+end)
