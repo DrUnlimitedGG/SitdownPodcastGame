59395b7dbd7c981a14a63b90a5ca0c38d73a28d8
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-19 21:35:48 -0400

    made the preset button to dim all of the lights

--- src/ServerScriptService/Lighting/PresetAll.server.lua
+++ src/ServerScriptService/Lighting/PresetAll.server.lua
@@ -3,9 +3,15 @@ local AllEnabled = game.ServerStorage.AllLights.AllEnabled
 local isChanging = game.ServerStorage.AllLights.IsChanging
 local module = require(game.ServerScriptService.CheckGroupRank)
 
-
 AllEnabled = true
 isChanging = false
+
+-- function for dimming the lights
+local function lightDim(light)
+	local TweenInfo = TweenInfo.new(2)
+	local tween = TweenService:Create(light,TweenInfo,{Brightness = 0})
+	tween:Play()
+end
 
 -- creates a folder to store all of light brightness values inside of ServerStorage
 local values = Instance.new("Folder")
@@ -44,6 +50,41 @@ local function lightBrighten(light)
 
 	tween:Play()
 end
+
+game.ReplicatedStorage.Lighting.AllOff.OnServerEvent:Connect(function(player)
+	-- Check if player is either a Co-Host, Host, or Developer in the group
+	local isAllowed = module.CheckRank(player)
+	if isAllowed then		
+		-- Makes it so that if someone else clicks while the lights are already changing it won't do anything
+		if isChanging == false then
+			isChanging = true
+			
+			-- If lights are on, then they turn off
+				print("Dimming lights!")
+				-- Searches workspace for all lights (with exceptions in the if statements) and spawns multiple tasks to dim each light
+				for i, v in pairs(game.Workspace:GetDescendants()) do
+					if v:IsA("Light") then
+						if (v.Parent.Parent.Name == "Spotlights") == false then
+							if (v.Parent.Parent.Name == "FirePlace") == false then
+								if (v.Parent.Parent.Name == "Television") == false then
+									if (v.Parent.Parent.Name == "Backstage") == false then
+										if (v.Name == "Ignore") == false then
+											task.spawn(lightDim,v)
+										end
+									end	
+								end	
+							end
+						end
+					end
+				end
+			-- Does the opposite if all lights are off				
+		-- If lights are already dimming/brightening then it will reject their input
+		elseif isChanging == true then
+			print("DNC, already changing")
+		end
+		
+	end
+end)
 
 game.ReplicatedStorage.Lighting.AllOn.OnServerEvent:Connect(function(player)
 	local isAllowed = module.CheckRank(player)
