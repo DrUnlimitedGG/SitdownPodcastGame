192289e33498b10392c18106e6b4e1a2150aeea7
DrUnlimitedGG <epicplayzquestions@gmail.com>
2024-07-20 13:04:58 -0400

    continued rewriting door script

--- src/ServerScriptService/DoorControl.server.lua
+++ src/ServerScriptService/DoorControl.server.lua
@@ -2,6 +2,17 @@ local tweenservice = game:GetService("TweenService")
 local event = game.ReplicatedStorage.Doors.Doors
 local knobEvent = game.ReplicatedStorage.Doors.Knobs
 
+function twistKnob(knob)
+	local twistInfo = {0.5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0}
+	local properties = {CFrame = knob.CFrame * CFrame.Angles(math.rad(60), 0, 0)}
+	local endProperties = {CFrame = knob.CFrame * CFrame.Angles(0,0,0)}
+	
+	
+	knobEvent:FireAllClients(knob, twistInfo, properties)
+	wait(4.25)
+	knobEvent:FireAllClients(knob, twistInfo, endProperties)
+	
+end
 
 function openDoor(door, hasAudio, openStyle, turnDegree, hasKnob, knob)
 	local doorRoot = door.PrimaryPart
@@ -16,7 +27,9 @@ function openDoor(door, hasAudio, openStyle, turnDegree, hasKnob, knob)
 	local DoorSwingInfo = {}
 	local closeInfo = {}
 	
-	
+	if hasKnob then
+		task.spawn(twistKnob,knob)
+	end
 	
 	if openStyle == "quad" then
 		DoorSwingInfo = {1.75, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0}
@@ -61,4 +74,14 @@ end
 local Door1 = workspace:FindFirstChild("BackstageHallwayDoor")
 local Door1CD = Door1:FindFirstChild("ClickDetector")
 
+local Door2 = workspace:FindFirstChild("StageDoor")
+local Door2CD = Door2:FindFirstChild("ClickDetector")
+
+Door1CD.MouseClick:Connect(function()
+	openDoor(Door1, true, "quad", 90, true, Door1.Union)
+end)
+
+Door2CD.MouseClick:Connect(function()
+	openDoor(Door2, true, "quad", 90, false)
+end)
 
